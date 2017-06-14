f64b6e8ade1cfa1a4175330525bc1f62749b122a
ddarriba <diego.darriba@h-its.org>
2017-06-14 02:37:04 +0200

    missing files

---
+++ src/main/java/com/retrocraft/block/repairer/ContainerRepairer.java
@@ -0,0 +1,232 @@
+package com.retrocraft.block.repairer;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.inventory.IContainerListener;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+/**
+ * User: brandon3055
+ * Date: 06/01/2015
+ *
+ * ContainerSmelting is used to link the client side gui to the server side inventory and it is where
+ * you add the slots holding items. It is also used to send server side data such as progress bars to the client
+ * for use in guis
+ */
+public class ContainerRepairer extends Container {
+
+	// Stores the tile entity instance for later use
+	private TileRepairer tileInventoryFurnace;
+
+	// These store cache values, used by the server to only update the client side tile entity when values have changed
+	private int [] cachedFields;
+
+	// must assign a slot index to each of the slots used by the GUI.
+	// For this container, we can see the furnace fuel, input, and output slots as well as the player inventory slots and the hotbar.
+	// Each time we add a Slot to the container using addSlotToContainer(), it automatically increases the slotIndex, which means
+	//  0 - 8 = hotbar slots (which will map to the InventoryPlayer slot numbers 0 - 8)
+	//  9 - 35 = player inventory slots (which map to the InventoryPlayer slot numbers 9 - 35)
+	//  36 - 39 = fuel slots (tileEntity 0 - 3)
+	//  40 - 44 = input slots (tileEntity 4 - 8)
+	//  45 - 49 = output slots (tileEntity 9 - 13)
+
+	public static final EntityEquipmentSlot[] VALID_EQUIPMENT_SLOTS = new EntityEquipmentSlot[] { 
+			EntityEquipmentSlot.HEAD, 
+			EntityEquipmentSlot.CHEST, 
+			EntityEquipmentSlot.LEGS, 
+			EntityEquipmentSlot.FEET, 
+			EntityEquipmentSlot.OFFHAND };
+	
+	private final int PLAYER_EQUIPMENT_SLOT_COUNT = 5;
+	private final int HOTBAR_SLOT_COUNT = 9;
+	private final int PLAYER_INVENTORY_ROW_COUNT = 3;
+	private final int PLAYER_INVENTORY_COLUMN_COUNT = 9;
+	private final int PLAYER_INVENTORY_SLOT_COUNT = PLAYER_INVENTORY_COLUMN_COUNT * PLAYER_INVENTORY_ROW_COUNT;
+	private final int VANILLA_SLOT_COUNT = HOTBAR_SLOT_COUNT + PLAYER_INVENTORY_SLOT_COUNT;
+
+	// slots for armor
+	private final int FIRST_ARMOR_SLOT_NUMBER = 36;
+	
+	public final int INPUT_SLOTS_COUNT = 1;
+	public final int OUTPUT_SLOTS_COUNT = 1;
+	public final int FURNACE_SLOTS_COUNT = INPUT_SLOTS_COUNT + OUTPUT_SLOTS_COUNT;
+
+	// slot index is the unique index for all slots in this container i.e. 0 - 35 for invPlayer then 36 - 49 for tileInventoryFurnace
+	private final int VANILLA_FIRST_SLOT_INDEX = 0;
+	private final int FIRST_INPUT_SLOT_INDEX = VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT;
+	private final int FIRST_OUTPUT_SLOT_INDEX = FIRST_INPUT_SLOT_INDEX + INPUT_SLOTS_COUNT;
+
+	// slot number is the slot number within each component; i.e. invPlayer slots 0 - 35, and tileInventoryFurnace slots 0 - 14
+	private final int FIRST_INPUT_SLOT_NUMBER = 0;
+	private final int FIRST_OUTPUT_SLOT_NUMBER = FIRST_INPUT_SLOT_NUMBER + INPUT_SLOTS_COUNT;
+
+	public ContainerRepairer(InventoryPlayer invPlayer, TileRepairer tileRepairer) {
+		this.tileInventoryFurnace = tileRepairer;
+
+		final int SLOT_X_SPACING = 18;
+		final int SLOT_Y_SPACING = 18;
+		final int HOTBAR_XPOS = 37;
+		final int HOTBAR_YPOS = 107;
+		// Add the players hotbar to the gui - the [xpos, ypos] location of each item
+		for (int x = 0; x < HOTBAR_SLOT_COUNT; x++) {
+			int slotNumber = x;
+			addSlotToContainer(new Slot(invPlayer, slotNumber, HOTBAR_XPOS + SLOT_X_SPACING * x, HOTBAR_YPOS));
+		}
+
+		final int PLAYER_INVENTORY_XPOS = 37;
+		final int PLAYER_INVENTORY_YPOS = 47;
+		// Add the rest of the players inventory to the gui
+		for (int y = 0; y < PLAYER_INVENTORY_ROW_COUNT; y++) {
+			for (int x = 0; x < PLAYER_INVENTORY_COLUMN_COUNT; x++) {
+				int slotNumber = HOTBAR_SLOT_COUNT + y * PLAYER_INVENTORY_COLUMN_COUNT + x;
+				int xpos = PLAYER_INVENTORY_XPOS + x * SLOT_X_SPACING;
+				int ypos = PLAYER_INVENTORY_YPOS + y * SLOT_Y_SPACING;
+				addSlotToContainer(new Slot(invPlayer, slotNumber,  xpos, ypos));
+			}
+		}
+		
+		final int PLAYER_EQUIPMENT_XPOS = 7;
+		final int PLAYER_EQUIPMENT_YPOS = 24;
+		for (int k = 0; k<PLAYER_EQUIPMENT_SLOT_COUNT; k++) {
+            final int armorType = k;
+            
+            addSlotToContainer(new Slot(invPlayer, k<4?(39 - k):40, 
+            		PLAYER_EQUIPMENT_XPOS, 
+            		PLAYER_EQUIPMENT_YPOS + k * 19) {
+                @Override
+                public int getSlotStackLimit () {
+                    
+                    return 1;
+                }
+                
+                @Override
+                public boolean isItemValid (ItemStack par1ItemStack) {
+                	EntityEquipmentSlot entityEquipmentSlot = VALID_EQUIPMENT_SLOTS[armorType];                    
+                    Item item = (par1ItemStack == null ? null : par1ItemStack.getItem());
+                    return item != null && item.isValidArmor(par1ItemStack, entityEquipmentSlot , invPlayer.player);
+                }
+            });
+        }
+
+		final int INPUT_SLOTS_XPOS = 37;
+		final int INPUT_SLOTS_YPOS = 17;
+		addSlotToContainer(new SlotRepairableInput(tileRepairer, FIRST_INPUT_SLOT_NUMBER, INPUT_SLOTS_XPOS, INPUT_SLOTS_YPOS));
+
+		final int OUTPUT_SLOTS_XPOS = 61;
+		final int OUTPUT_SLOTS_YPOS = 17;
+		addSlotToContainer(new SlotOutput(tileRepairer, FIRST_OUTPUT_SLOT_NUMBER, OUTPUT_SLOTS_XPOS, OUTPUT_SLOTS_YPOS));
+	}
+
+	public boolean repairItem()
+	{
+		return tileInventoryFurnace.repairItem();
+	}
+	// Checks each tick to make sure the player is still able to access the inventory and if not closes the gui
+	@Override
+	public boolean canInteractWith(EntityPlayer player)
+	{
+		return tileInventoryFurnace.isUsableByPlayer(player);
+	}
+
+	// This is where you specify what happens when a player shift clicks a slot in the gui
+	//  (when you shift click a slot in the TileEntity Inventory, it moves it to the first available position in the hotbar and/or
+	//    player inventory.  When you you shift-click a hotbar or player inventory item, it moves it to the first available
+	//    position in the TileEntity inventory - either input or fuel as appropriate for the item you clicked)
+	// At the very least you must override this and return EMPTY_ITEM or the game will crash when the player shift clicks a slot
+	// returns EMPTY_ITEM if the source slot is empty, or if none of the source slot items could be moved.
+	//   otherwise, returns a copy of the source stack
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer player, int sourceSlotIndex)
+	{
+		Slot sourceSlot = (Slot)inventorySlots.get(sourceSlotIndex);
+		if (sourceSlot == null || !sourceSlot.getHasStack()) return ItemStack.EMPTY;  //EMPTY_ITEM
+		ItemStack sourceStack = sourceSlot.getStack();
+		ItemStack copyOfSourceStack = sourceStack.copy();
+
+		// Check if the slot clicked is one of the vanilla container slots
+		if (sourceSlotIndex >= VANILLA_FIRST_SLOT_INDEX && sourceSlotIndex < VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT) {
+			// This is a vanilla container slot so merge the stack into one of the furnace slots
+			// If the stack is smeltable try to merge merge the stack into the input slots
+			if (!TileRepairer.getSmeltingResultForItem(sourceStack).isEmpty()){  //isEmptyItem
+				if (!mergeItemStack(sourceStack, FIRST_INPUT_SLOT_INDEX, FIRST_INPUT_SLOT_INDEX + INPUT_SLOTS_COUNT, false)){
+					return ItemStack.EMPTY;  //EMPTY_ITEM;
+				}
+			} else {
+				return ItemStack.EMPTY;  //EMPTY_ITEM;
+			}
+		} else {
+			System.err.print("Invalid slotIndex:" + sourceSlotIndex);
+			return ItemStack.EMPTY;  //EMPTY_ITEM;
+		}
+
+		// If stack size == 0 (the entire stack was moved) set slot contents to null
+		if (sourceStack.getCount() == 0) {  //getStackSize()
+			sourceSlot.putStack(ItemStack.EMPTY);  // Empty Item
+		} else {
+			sourceSlot.onSlotChanged();
+		}
+
+		sourceSlot.onTake(player, sourceStack);  // onPickupFromSlot()
+		return copyOfSourceStack;
+	}
+
+	/* Client Synchronization */
+
+	// This is where you check if any values have changed and if so send an update to any clients accessing this container
+	// The container itemstacks are tested in Container.detectAndSendChanges, so we don't need to do that
+	// We iterate through all of the TileEntity Fields to find any which have changed, and send them.
+	// You don't have to use fields if you don't wish to; just manually match the ID in sendProgressBarUpdate with the value in
+	//   updateProgressBar()
+	// The progress bar values are restricted to shorts.  If you have a larger value (eg int), it's not a good idea to try and split it
+	//   up into two shorts because the progress bar values are sent independently, and unless you add synchronisation logic at the
+	//   receiving side, your int value will be wrong until the second short arrives.  Use a custom packet instead.
+	@Override
+	public void detectAndSendChanges() {
+		super.detectAndSendChanges();
+		if (!tileInventoryFurnace.getStackInSlot(1).isEmpty())
+			for (IContainerListener listener : this.listeners) {
+				System.out.println("[RETROCRAFT] Container: Notify listeners!");
+			}
+	}
+
+	// Called when a progress bar update is received from the server. The two values (id and data) are the same two
+	// values given to sendProgressBarUpdate.  In this case we are using fields so we just pass them to the tileEntity.
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void updateProgressBar(int id, int data) {
+		tileInventoryFurnace.setField(id, data);
+	}
+
+	// SlotSmeltableInput is a slot for input items
+	public class SlotRepairableInput extends Slot {
+		public SlotRepairableInput(IInventory inventoryIn, int index, int xPosition, int yPosition) {
+			super(inventoryIn, index, xPosition, yPosition);
+		}
+
+		// if this function returns false, the player won't be able to insert the given item into this slot
+		@Override
+		public boolean isItemValid(ItemStack stack) {
+			return TileRepairer.isItemValidForInputSlot(stack);
+		}
+	}
+
+	// SlotOutput is a slot that will not accept any items
+	public class SlotOutput extends Slot {
+		public SlotOutput(IInventory inventoryIn, int index, int xPosition, int yPosition) {
+			super(inventoryIn, index, xPosition, yPosition);
+		}
+
+		// if this function returns false, the player won't be able to insert the given item into this slot
+		@Override
+		public boolean isItemValid(ItemStack stack) {
+			return TileRepairer.isItemValidForOutputSlot(stack);
+		}
+	}
+}
---
+++ src/main/java/com/retrocraft/block/repairer/TileRepairer.java
@@ -0,0 +1,416 @@
+package com.retrocraft.block.repairer;
+
+import java.util.Arrays;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagIntArray;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityFurnace;
+import net.minecraft.util.ITickable;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.EnumSkyBlock;
+
+/**
+ * User: brandon3055
+ * Date: 06/01/2015
+ *
+ * TileInventorySmelting is an advanced sided inventory that works like a vanilla furnace except that it has 5 input and output slots,
+ * 4 fuel slots and cooks at up to four times the speed.
+ * The input slots are used sequentially rather than in parallel, i.e. the first slot cooks, then the second, then the third, etc
+ * The fuel slots are used in parallel.  The more slots burning in parallel, the faster the cook time.
+ * The code is heavily based on TileEntityFurnace.
+ */
+public class TileRepairer extends TileEntity implements IInventory, ITickable {
+	// Create and initialize the itemStacks variable that will store store the itemStacks
+	public static final int INPUT_SLOTS_COUNT = 1;
+	public static final int OUTPUT_SLOTS_COUNT = 1;
+	public static final int TOTAL_SLOTS_COUNT = INPUT_SLOTS_COUNT + OUTPUT_SLOTS_COUNT;
+
+	public static final int FIRST_INPUT_SLOT = 0;
+	public static final int FIRST_OUTPUT_SLOT = FIRST_INPUT_SLOT + INPUT_SLOTS_COUNT;
+
+	private ItemStack[] itemStacks;
+
+	public TileRepairer()
+	{
+		itemStacks = new ItemStack[TOTAL_SLOTS_COUNT];
+		clear();
+	}
+
+	// This method is called every tick to update the tile entity, i.e.
+	// - see if the fuel has run out, and if so turn the furnace "off" and slowly uncook the current item (if any)
+	// - see if any of the items have finished smelting
+	// It runs both on the server and the client.
+	@Override
+	public void update() {
+		// The block update (for renderer) is only required on client side, but the lighting is required on both, since
+		//    the client needs it for rendering and the server needs it for crop growth etc
+		System.out.println("[RETROCRAFT] Tile: Update request");
+		if (itemStacks[FIRST_OUTPUT_SLOT].getCount() == 1) {
+			if (world.isRemote) {
+				System.out.println("[RETROCRAFT] Tile: Update performed");
+				IBlockState iblockstate = this.world.getBlockState(pos);
+				repairItem();
+				final int FLAGS = 3;  // I'm not sure what these flags do, exactly.
+				world.notifyBlockUpdate(pos, iblockstate, iblockstate, FLAGS);
+			}
+			world.checkLightFor(EnumSkyBlock.BLOCK, pos);
+		}
+	}
+
+
+//public void repair (EntityPlayer player, int cost) throws Exception {
+//        
+//        final ItemStack itemStack = tableInventory.getStackInSlot(0);
+//        
+//        if (itemStack == null)
+//            return;
+//            
+//        boolean flag = !itemStack.hasTagCompound() || !itemStack.getTagCompound().hasKey("charge");
+//        
+//        if ((!itemStack.isItemEnchanted() || cost == 0) && flag)
+//            return;
+//            
+//        if (canPurchase(player, cost)) {
+//            int maxCost = repairCostMax();
+//            double percAmnt = cost / (double) maxCost;
+//            
+//            int remain = itemStack.getItemDamageForDisplay();
+//            double newDamage = remain - remain * percAmnt;
+//            newDamage = (newDamage <= 0) ? 0 : newDamage;
+//            
+//            itemStack.setItemDamage((int) newDamage);
+//            if (!player.capabilities.isCreativeMode)
+//                player.addExperienceLevel(-cost);
+//                
+//        }
+//        
+//        onCraftMatrixChanged(tableInventory);
+//}
+
+	/**
+	 * checks that there is an item to be smelted in one of the input slots and that there is room for the result in the output slots
+	 * If desired, performs the smelt
+	 * @param performSmelt if true, perform the smelt.  if false, check whether smelting is possible, but don't change the inventory
+	 * @return false if no items can be smelted, true otherwise
+	 */
+	public boolean repairItem()
+	{
+		System.out.println("[RETROCRAFT] Tile: Repair request");
+		final ItemStack inputStack = itemStacks[FIRST_INPUT_SLOT];
+		if (inputStack.isEmpty())
+			return false;
+		
+		System.out.println("[RETROCRAFT] Tile: Repair done");
+		inputStack.setItemDamage(0);
+		itemStacks[FIRST_OUTPUT_SLOT] = inputStack.copy();
+		
+		markDirty();
+		return true;
+	}
+
+	// returns the smelting result for the given stack. Returns null if the given stack can not be smelted
+	public static ItemStack getSmeltingResultForItem(ItemStack stack) { return FurnaceRecipes.instance().getSmeltingResult(stack); }
+
+	// returns the number of ticks the given item will burn. Returns 0 if the given item is not a valid fuel
+	public static short getItemBurnTime(ItemStack stack)
+	{
+		int burntime = TileEntityFurnace.getItemBurnTime(stack);  // just use the vanilla values
+		return (short)MathHelper.clamp(burntime, 0, Short.MAX_VALUE);
+	}
+
+	// Gets the number of slots in the inventory
+	@Override
+	public int getSizeInventory() {
+		return itemStacks.length;
+	}
+
+	// returns true if all of the slots in the inventory are empty
+	@Override
+	public boolean isEmpty()
+	{
+		for (ItemStack itemstack : itemStacks) {
+			if (!itemstack.isEmpty()) {  // isEmpty()
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	// Gets the stack in the given slot
+	@Override
+	public ItemStack getStackInSlot(int i) {
+		return itemStacks[i];
+	}
+
+	/**
+	 * Removes some of the units from itemstack in the given slot, and returns as a separate itemstack
+	 * @param slotIndex the slot number to remove the items from
+	 * @param count the number of units to remove
+	 * @return a new itemstack containing the units removed from the slot
+	 */
+	@Override
+	public ItemStack decrStackSize(int slotIndex, int count) {
+		ItemStack itemStackInSlot = getStackInSlot(slotIndex);
+		if (itemStackInSlot.isEmpty()) return ItemStack.EMPTY;  //isEmpty(), EMPTY_ITEM
+
+		ItemStack itemStackRemoved;
+		if (itemStackInSlot.getCount() <= count) { //getStackSize
+			itemStackRemoved = itemStackInSlot;
+			setInventorySlotContents(slotIndex, ItemStack.EMPTY); // EMPTY_ITEM
+		} else {
+			itemStackRemoved = itemStackInSlot.splitStack(count);
+			if (itemStackInSlot.getCount() == 0) { //getStackSize
+				setInventorySlotContents(slotIndex, ItemStack.EMPTY); //EMPTY_ITEM
+			}
+		}
+		markDirty();
+		return itemStackRemoved;
+	}
+
+	// overwrites the stack in the given slotIndex with the given stack
+	@Override
+	public void setInventorySlotContents(int slotIndex, ItemStack itemstack) {
+		itemStacks[slotIndex] = itemstack;
+		if (!itemstack.isEmpty() && itemstack.getCount() > getInventoryStackLimit()) {  // isEmpty();  getStackSize()
+			itemstack.setCount(getInventoryStackLimit());  //setStackSize()
+		}
+		markDirty();
+	}
+
+	// This is the maximum number if items allowed in each slot
+	// This only affects things such as hoppers trying to insert items you need to use the container to enforce this for players
+	// inserting items via the gui
+	@Override
+	public int getInventoryStackLimit() {
+		return 1;
+	}
+
+	// Return true if the given player is able to use this block. In this case it checks that
+	// 1) the world tileentity hasn't been replaced in the meantime, and
+	// 2) the player isn't too far away from the centre of the block
+	@Override
+	public boolean isUsableByPlayer(EntityPlayer player) {
+		if (this.world.getTileEntity(this.pos) != this) return false;
+		final double X_CENTRE_OFFSET = 0.5;
+		final double Y_CENTRE_OFFSET = 0.5;
+		final double Z_CENTRE_OFFSET = 0.5;
+		final double MAXIMUM_DISTANCE_SQ = 8.0 * 8.0;
+		return player.getDistanceSq(pos.getX() + X_CENTRE_OFFSET, pos.getY() + Y_CENTRE_OFFSET, pos.getZ() + Z_CENTRE_OFFSET) < MAXIMUM_DISTANCE_SQ;
+	}
+
+	// Return true if the given stack is allowed to be inserted in the given slot
+	// Unlike the vanilla furnace, we allow anything to be placed in the fuel slots
+	static public boolean isItemValidForInputSlot(ItemStack itemStack)
+	{
+		return itemStack.isItemEnchantable() || itemStack.isItemEnchanted();
+	}
+
+	// Return true if the given stack is allowed to be inserted in the given slot
+	// Unlike the vanilla furnace, we allow anything to be placed in the fuel slots
+	static public boolean isItemValidForOutputSlot(ItemStack itemStack)
+	{
+		return false;
+	}
+
+	//------------------------------
+
+	// This is where you save any data that you don't want to lose when the tile entity unloads
+	// In this case, it saves the state of the furnace (burn time etc) and the itemstacks stored in the fuel, input, and output slots
+	@Override
+	public NBTTagCompound writeToNBT(NBTTagCompound parentNBTTagCompound)
+	{
+		super.writeToNBT(parentNBTTagCompound); // The super call is required to save and load the tiles location
+
+//		// Save the stored item stacks
+
+		// to use an analogy with Java, this code generates an array of hashmaps
+		// The itemStack in each slot is converted to an NBTTagCompound, which is effectively a hashmap of key->value pairs such
+		//   as slot=1, id=2353, count=1, etc
+		// Each of these NBTTagCompound are then inserted into NBTTagList, which is similar to an array.
+		NBTTagList dataForAllSlots = new NBTTagList();
+		for (int i = 0; i < this.itemStacks.length; ++i) {
+			if (!this.itemStacks[i].isEmpty()) {  //isEmpty()
+				NBTTagCompound dataForThisSlot = new NBTTagCompound();
+				dataForThisSlot.setByte("Slot", (byte) i);
+				this.itemStacks[i].writeToNBT(dataForThisSlot);
+				dataForAllSlots.appendTag(dataForThisSlot);
+			}
+		}
+		// the array of hashmaps is then inserted into the parent hashmap for the container
+		parentNBTTagCompound.setTag("Items", dataForAllSlots);
+
+		return parentNBTTagCompound;
+	}
+
+	// This is where you load the data that you saved in writeToNBT
+	@Override
+	public void readFromNBT(NBTTagCompound nbtTagCompound)
+	{
+		super.readFromNBT(nbtTagCompound); // The super call is required to save and load the tiles location
+		final byte NBT_TYPE_COMPOUND = 10;       // See NBTBase.createNewByType() for a listing
+		NBTTagList dataForAllSlots = nbtTagCompound.getTagList("Items", NBT_TYPE_COMPOUND);
+
+		Arrays.fill(itemStacks, ItemStack.EMPTY);           // set all slots to empty EMPTY_ITEM
+		for (int i = 0; i < dataForAllSlots.tagCount(); ++i) {
+			NBTTagCompound dataForOneSlot = dataForAllSlots.getCompoundTagAt(i);
+			byte slotNumber = dataForOneSlot.getByte("Slot");
+			if (slotNumber >= 0 && slotNumber < this.itemStacks.length) {
+				this.itemStacks[slotNumber] = new ItemStack(dataForOneSlot);
+			}
+		}
+	}
+
+//	// When the world loads from disk, the server needs to send the TileEntity information to the client
+//	//  it uses getUpdatePacket(), getUpdateTag(), onDataPacket(), and handleUpdateTag() to do this
+  @Override
+  @Nullable
+  public SPacketUpdateTileEntity getUpdatePacket()
+  {
+	  System.out.println("[RETROCRAFT] Tile: SPacketUpdateTileEntity");
+    NBTTagCompound updateTagDescribingTileEntityState = getUpdateTag();
+    final int METADATA = 0;
+    return new SPacketUpdateTileEntity(this.pos, METADATA, updateTagDescribingTileEntityState);
+  }
+
+  @Override
+  public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt) {
+    NBTTagCompound updateTagDescribingTileEntityState = pkt.getNbtCompound();
+    handleUpdateTag(updateTagDescribingTileEntityState);
+    System.out.println("[RETROCRAFT] Tile: ondatapacket");
+  }
+
+  /* Creates a tag containing the TileEntity information, used by vanilla to transmit from server to client
+     Warning - although our getUpdatePacket() uses this method, vanilla also calls it directly, so don't remove it.
+   */
+  @Override
+  public NBTTagCompound getUpdateTag()
+  {
+	  System.out.println("[RETROCRAFT] Tile: getUpdateTag");
+		NBTTagCompound nbtTagCompound = new NBTTagCompound();
+		writeToNBT(nbtTagCompound);
+    return nbtTagCompound;
+  }
+
+  /* Populates this TileEntity with information from the tag, used by vanilla to transmit from server to client
+   Warning - although our onDataPacket() uses this method, vanilla also calls it directly, so don't remove it.
+ */
+  @Override
+  public void handleUpdateTag(NBTTagCompound tag)
+  {
+	  System.out.println("[RETROCRAFT] Tile: handleupdatetag");
+    this.readFromNBT(tag);
+  }
+  //------------------------
+
+	// set all slots to empty
+	@Override
+	public void clear() {
+		Arrays.fill(itemStacks, ItemStack.EMPTY);  //EMPTY_ITEM
+	}
+
+	// will add a key for this container to the lang file so we can name it in the GUI
+	@Override
+	public String getName() {
+		return "tile.repairer.name";
+	}
+
+	@Override
+	public boolean hasCustomName() {
+		return false;
+	}
+
+	// standard code to look up what the human-readable name is
+  @Nullable
+  @Override
+  public ITextComponent getDisplayName() {
+		return this.hasCustomName() ? new TextComponentString(this.getName()) : new TextComponentTranslation(this.getName());
+	}
+
+//	// Fields are used to send non-inventory information from the server to interested clients
+//	// The container code caches the fields and sends the client any fields which have changed.
+//	// The field ID is limited to byte, and the field value is limited to short. (if you use more than this, they get cast down
+//	//   in the network packets)
+//	// If you need more than this, or shorts are too small, use a custom packet in your container instead.
+//
+//	private static final byte COOK_FIELD_ID = 0;
+//	private static final byte FIRST_BURN_TIME_REMAINING_FIELD_ID = 1;
+//	private static final byte FIRST_BURN_TIME_INITIAL_FIELD_ID = FIRST_BURN_TIME_REMAINING_FIELD_ID + (byte)FUEL_SLOTS_COUNT;
+//	private static final byte NUMBER_OF_FIELDS = FIRST_BURN_TIME_INITIAL_FIELD_ID + (byte)FUEL_SLOTS_COUNT;
+//
+	@Override
+	public int getField(int id) {
+//		if (id == COOK_FIELD_ID) return cookTime;
+//		if (id >= FIRST_BURN_TIME_REMAINING_FIELD_ID && id < FIRST_BURN_TIME_REMAINING_FIELD_ID + FUEL_SLOTS_COUNT) {
+//			return burnTimeRemaining[id - FIRST_BURN_TIME_REMAINING_FIELD_ID];
+//		}
+//		if (id >= FIRST_BURN_TIME_INITIAL_FIELD_ID && id < FIRST_BURN_TIME_INITIAL_FIELD_ID + FUEL_SLOTS_COUNT) {
+//			return burnTimeInitialValue[id - FIRST_BURN_TIME_INITIAL_FIELD_ID];
+//		}
+//		System.err.println("Invalid field ID in TileInventorySmelting.getField:" + id);
+		return 0;
+	}
+
+	@Override
+	public void setField(int id, int value)
+	{
+//		if (id == COOK_FIELD_ID) {
+//			cookTime = (short)value;
+//		} else if (id >= FIRST_BURN_TIME_REMAINING_FIELD_ID && id < FIRST_BURN_TIME_REMAINING_FIELD_ID + FUEL_SLOTS_COUNT) {
+//			burnTimeRemaining[id - FIRST_BURN_TIME_REMAINING_FIELD_ID] = value;
+//		} else if (id >= FIRST_BURN_TIME_INITIAL_FIELD_ID && id < FIRST_BURN_TIME_INITIAL_FIELD_ID + FUEL_SLOTS_COUNT) {
+//			burnTimeInitialValue[id - FIRST_BURN_TIME_INITIAL_FIELD_ID] = value;
+//		} else {
+//			System.err.println("Invalid field ID in TileInventorySmelting.setField:" + id);
+//		}
+	}
+
+	@Override
+	public int getFieldCount() {
+		return 0;
+	}
+
+	// -----------------------------------------------------------------------------------------------------------
+	// The following methods are not needed for this example but are part of IInventory so they must be implemented
+
+	// Unused unless your container specifically uses it.
+	// Return true if the given stack is allowed to go in the given slot
+	@Override
+	public boolean isItemValidForSlot(int slotIndex, ItemStack itemstack) {
+		return false;
+	}
+
+	/**
+	 * This method removes the entire contents of the given slot and returns it.
+	 * Used by containers such as crafting tables which return any items in their slots when you close the GUI
+	 * @param slotIndex
+	 * @return
+	 */
+	@Override
+	public ItemStack removeStackFromSlot(int slotIndex) {
+		ItemStack itemStack = getStackInSlot(slotIndex);
+		if (!itemStack.isEmpty()) setInventorySlotContents(slotIndex, ItemStack.EMPTY);  //isEmpty();  EMPTY_ITEM
+		return itemStack;
+	}
+
+	@Override
+	public void openInventory(EntityPlayer player) {}
+
+	@Override
+	public void closeInventory(EntityPlayer player) {}
+
+}
