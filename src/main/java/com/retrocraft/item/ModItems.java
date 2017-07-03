dc4169e340af3fe9c1299f7834e8d76f5b577509
ddarriba <diego.darriba@h-its.org>
2017-07-03 17:52:57 +0200

    mechanical/magical cores

--- src/main/java/com/retrocraft/item/ModItems.java
+++ src/main/java/com/retrocraft/item/ModItems.java
@@ -19,13 +19,16 @@ public class ModItems {
   public static ItemOre dustManolite;
   public static ItemOre dustManolazium;
   public static ItemOre gemOctirion;
-  
+
 	/* ingot */
 	public static ItemOre ingotManolium;
 	public static ItemOre ingotManolazium;
-	
+
+  public static ItemOre mechanicalCore;
+	public static ItemOre magicalCore;
+
 	public static ItemSword manoliumSword;
-	
+
 	/* hammer */
 	public static ToolHammer stoneHammer;
 	public static ToolHammer ironHammer;
@@ -33,53 +36,56 @@ public class ModItems {
 	public static ToolHammer diamondHammer;
 	public static ToolHammer emeraldHammer;
 	public static ToolHammer manoliumHammer;
-	
+
 	public static ToolExcavator manoliumExcavator;
 	public static ToolEverything manoliumETool;
 	public static ToolStreamAxe manoliumStreamAxe;
 	public static ItemSword manolaziumSword;
-	
+
 	public static ItemWoodenBucket woodenBucket;
 	public static ItemWoodenBucket woodenWaterBucket;
 	public static ItemWoodenMilkBucket woodenMilkBucket;
-		
+
 	public static void init() {
 	  dustManolite = register(new ItemOre("dust_manolite", "dustManolite").setCreativeTab(RetroCraft.creativeTab));
 	  dustManolazium = register(new ItemOre("dust_manolazium", "dustManolazium").setCreativeTab(RetroCraft.creativeTab));
 	  gemOctirion = register(new ItemOre("gem_octirion", "gemOctirion").setCreativeTab(RetroCraft.creativeTab));
-	  
+
 		ingotManolium = register(new ItemOre("ingot_manolium", "ingotManolium").setCreativeTab(RetroCraft.creativeTab));
 		ingotManolazium = register(new ItemOre("ingot_manolazium", "ingotManolazium").setCreativeTab(RetroCraft.creativeTab));
-		
+
+    mechanicalCore = register(new ItemOre("mechanical_core", "mechanicalCore").setCreativeTab(RetroCraft.creativeTab));
+    magicalCore = register(new ItemOre("magical_core", "magicalCore").setCreativeTab(RetroCraft.creativeTab));
+
 		manoliumSword = register(new ItemSword(RetroCraft.manoliumToolMaterial, "sword_manolium").setCreativeTab(RetroCraft.creativeTab));
-		
+
 		stoneHammer = register(new ToolHammer(ToolMaterial.STONE, "hammer_stone").setCreativeTab(RetroCraft.creativeTab));
 		ironHammer = register(new ToolHammer(RetroCraft.manoliumToolMaterial, "hammer_iron").setCreativeTab(RetroCraft.creativeTab));
 		goldHammer = register(new ToolHammer(RetroCraft.manoliumToolMaterial, "hammer_gold").setCreativeTab(RetroCraft.creativeTab));
 		diamondHammer = register(new ToolHammer(RetroCraft.manoliumToolMaterial, "hammer_diamond").setCreativeTab(RetroCraft.creativeTab));
 		emeraldHammer = register(new ToolHammer(RetroCraft.manoliumToolMaterial, "hammer_emerald").setCreativeTab(RetroCraft.creativeTab));
 		manoliumHammer = register(new ToolHammer(RetroCraft.manoliumToolMaterial, "hammer_manolium").setCreativeTab(RetroCraft.creativeTab));
-		
+
 		manoliumExcavator = register(new ToolExcavator(RetroCraft.manoliumToolMaterial, "excavator_manolium").setCreativeTab(RetroCraft.creativeTab));
 		manoliumStreamAxe = register(new ToolStreamAxe(ToolMaterial.DIAMOND, "streamaxe_manolium").setCreativeTab(RetroCraft.creativeTab));
 		manoliumETool = register(new ToolEverything(RetroCraft.manoliumToolMaterial, "etool_manolium").setCreativeTab(RetroCraft.creativeTab));
 		manolaziumSword = register(new ItemSword(RetroCraft.manolaziumToolMaterial, "sword_manolazium").setCreativeTab(RetroCraft.creativeTab));
-		
+
 		woodenBucket = register(new ItemWoodenBucket("wooden_bucket").setCreativeTab(RetroCraft.creativeTab));
 		woodenMilkBucket = register(new ItemWoodenMilkBucket("wooden_bucket_milk").setCreativeTab(RetroCraft.creativeTab));
 	}
-	
+
 	private static <T extends Item> T register(T item) {
 		GameRegistry.register(item);
-		
+
 		if(item instanceof ItemModelProvider) {
 			((ItemModelProvider)item).registerItemModel(item);
 		}
-		
+
 		if (item instanceof ItemOreDict) {
 			((ItemOreDict)item).initOreDict();
 		}
-		
+
 		return item;
 	}
 }
--- src/main/java/com/retrocraft/recipe/RetrocraftRecipes.java
+++ src/main/java/com/retrocraft/recipe/RetrocraftRecipes.java
@@ -10,60 +10,71 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.OreDictionary;
 
-public class RetrocraftRecipes 
+public class RetrocraftRecipes
 {
-	public static void init() 
+	public static void init()
 	{
-		GameRegistry.addSmelting(ModItems.dustManolite, 
+		GameRegistry.addSmelting(ModItems.dustManolite,
 		                         new ItemStack(ModItems.ingotManolium), 0.7f);
-		GameRegistry.addSmelting(ModItems.dustManolazium, 
+		GameRegistry.addSmelting(ModItems.dustManolazium,
                              new ItemStack(ModItems.ingotManolazium), 0.7f);
-		
-		GameRegistry.addSmelting(ModBlocks.oreManolite, 
+
+		GameRegistry.addSmelting(ModBlocks.oreManolite,
 		                         new ItemStack(ModItems.ingotManolium), 0.7f);
 
 		GameRegistry.addShapelessRecipe(
-				new ItemStack(ModItems.dustManolazium), 
+				new ItemStack(ModItems.dustManolazium),
 				new Object[] { ModItems.dustManolite, Items.EMERALD, Items.DIAMOND, Items.REDSTONE });
-		
+
 		GameRegistry.addShapedRecipe(
-				new ItemStack(ModItems.dustManolazium), "E", "M", "E", 
-				'M', ModItems.dustManolite, 
+				new ItemStack(ModItems.dustManolazium), "E", "M", "E",
+				'M', ModItems.dustManolite,
 				'E', Items.EMERALD);
-		 
+
 		GameRegistry.addShapedRecipe(
-				new ItemStack(Items.DIAMOND_PICKAXE), "III", " S ", " S ", 
-				'I', ModItems.ingotManolium, 
+				new ItemStack(Items.DIAMOND_PICKAXE), "III", " S ", " S ",
+				'I', ModItems.ingotManolium,
 				'S', Items.STICK);
-		
+
 		GameRegistry.addShapedRecipe(
-				new ItemStack(ModItems.manoliumSword), "I", "I", "S", 
-				'I', ModItems.ingotManolium, 
+				new ItemStack(ModItems.manoliumSword), "I", "I", "S",
+				'I', ModItems.ingotManolium,
 				'S', Items.STICK);
-		
+
 		GameRegistry.addShapedRecipe(
-				new ItemStack(ModBlocks.pedestalManolium), "OOO", " S ", " S ", 
-				'O', ModItems.ingotManolium, 
+				new ItemStack(ModItems.mechanicalCore), "MMM", "MDM", "MMM",
+				'M', ModItems.ingotManolium,
+				'D', Items.DIAMOND);
+
+		GameRegistry.addShapedRecipe(
+				new ItemStack(ModItems.magicalCore), "OOO", "ODO", "OOO",
+				'O', ModItems.gemOctirion,
+				'E', Items.EMERALD);
+
+		GameRegistry.addShapedRecipe(
+				new ItemStack(ModBlocks.pedestalManolium), "OOO", " M ", " S ",
+				'O', ModItems.ingotManolium,
+				'M', ModItems.mechanicalCore,
 				'S', Blocks.STONEBRICK);
-		
+
 		GameRegistry.addShapedRecipe(
-				new ItemStack(ModItems.manolaziumSword), "I", "I", "S", 
-				'I', ModItems.ingotManolazium, 
+				new ItemStack(ModItems.manolaziumSword), "I", "I", "S",
+				'I', ModItems.ingotManolazium,
 				'S', Items.STICK);
-		
+
 		GameRegistry.addShapedRecipe(
-        new ItemStack(ModItems.woodenBucket), "W W", "WSW", 
-        'W', Blocks.PLANKS, 
+        new ItemStack(ModItems.woodenBucket), "W W", "WSW",
+        'W', Blocks.PLANKS,
         'S', Blocks.WOODEN_SLAB);
-		
+
 		if(!RetroCraft.getConfig().creativeModeOnly) {
       GameRegistry.addShapedRecipe(
-          new ItemStack(ModBlocks.blockWaystone), " S ", "SWS", "OOO", 
-          'S', Blocks.STONEBRICK, 
-          'W', Items.ENDER_EYE, 
+          new ItemStack(ModBlocks.blockWaystone), " S ", "SWS", "OOO",
+          'S', Blocks.STONEBRICK,
+          'W', Items.ENDER_EYE,
           'O', Blocks.OBSIDIAN);
 		}
-		
+
 		/* Crusher */
 		GrinderRecipeRegistry.addRecipe(OreDictionary.getOres("oreOctirion", false), OreDictionary.getOres("gemOctirion", false));
 		GrinderRecipeRegistry.addRecipe(OreDictionary.getOres("oreNickel", false), OreDictionary.getOres("dustNickel", false));
@@ -75,7 +86,7 @@ public class RetrocraftRecipes
     GrinderRecipeRegistry.addRecipe(OreDictionary.getOres("oreRedstone", false), OreDictionary.getOres("dustRedstone", false));
     GrinderRecipeRegistry.addRecipe(new ItemStack(Blocks.GRAVEL), new ItemStack(Items.FLINT));
     GrinderRecipeRegistry.addRecipe(new ItemStack(Items.BLAZE_ROD), new ItemStack(Items.BLAZE_POWDER, 4));
-    
+
     GrinderRecipeRegistry.registerSearchCase(new SearchCase("denseore",  8, "dust"));
     GrinderRecipeRegistry.registerSearchCase(new SearchCase("gem",       1, "dust"));
     GrinderRecipeRegistry.registerSearchCase(new SearchCase("ingot",     1, "dust"));
@@ -83,7 +94,7 @@ public class RetrocraftRecipes
     GrinderRecipeRegistry.registerSearchCase(new SearchCase("orePoor",   4, "nugget"));
     GrinderRecipeRegistry.registerSearchCase(new SearchCase("ore",       2, "dust"));
     GrinderRecipeRegistry.registerSearchCase(new SearchCase("ore",       2, "gem"));
-    
+
     SmelterRecipeRegistry.addRecipe(OreDictionary.getOres("dustManolite", false), OreDictionary.getOres("ingotManolium", false));
     SmelterRecipeRegistry.addRecipe(OreDictionary.getOres("dustManolazium", false), OreDictionary.getOres("ingotManolazium", false));
     SmelterRecipeRegistry.addRecipe(OreDictionary.getOres("dustGold", false), OreDictionary.getOres("ingotGold", false));
@@ -91,11 +102,11 @@ public class RetrocraftRecipes
     SmelterRecipeRegistry.addRecipe(OreDictionary.getOres("dustDiamond", false), OreDictionary.getOres("gemDiamond", false));
     SmelterRecipeRegistry.addRecipe(OreDictionary.getOres("dustEmerald", false), OreDictionary.getOres("gemEmerald", false));
     SmelterRecipeRegistry.addRecipe(OreDictionary.getOres("dustLapis", false), OreDictionary.getOres("gemLapis", false));
-    
+
     SmelterRecipeRegistry.registerSearchCase(new SearchCase("dust", 1, "ingot"));
     SmelterRecipeRegistry.registerSearchCase(new SearchCase("dust", 1, "gem"));
 	}
-	
+
   public static class SearchCase
   {
 
