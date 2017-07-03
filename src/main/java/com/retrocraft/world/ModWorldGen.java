21ac6e0ce3f82608e94f0055bedcb7cf4bf90fac
ddarriba <diego.darriba@h-its.org>
2017-07-03 15:29:54 +0200

    Octirion world gen

--- src/main/java/com/retrocraft/world/ModWorldGen.java
+++ src/main/java/com/retrocraft/world/ModWorldGen.java
@@ -2,6 +2,7 @@ package com.retrocraft.world;
 
 import java.util.Random;
 
+import com.retrocraft.RCConfig;
 import com.retrocraft.block.ModBlocks;
 
 import net.minecraft.block.state.IBlockState;
@@ -15,37 +16,68 @@ import net.minecraftforge.fml.common.IWorldGenerator;
 public class ModWorldGen implements IWorldGenerator {
 
 	@Override
-	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator,
-			IChunkProvider chunkProvider) {
-		if (world.provider.getDimension() == 0) { // the overworld
+	public void generate(Random random,
+	                     int chunkX,
+											 int chunkZ,
+											 World world,
+											 IChunkGenerator chunkGenerator,
+											 IChunkProvider chunkProvider)
+  {
+		if (world.provider.getDimension() == 0)
+		{
+			 // the overworld
 			generateOverworld(
-					random, 
-					chunkX, 
-					chunkZ, 
-					world, 
-					chunkGenerator, 
+					random,
+					chunkX,
+					chunkZ,
+					world,
+					chunkGenerator,
 					chunkProvider);
 		}
 	}
 
-	private void generateOverworld(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator,
-			IChunkProvider chunkProvider) {
-		generateOre(ModBlocks.oreManolite.getDefaultState(), 
-				world, 
-				random, 
-				chunkX * 16, 
-				chunkZ * 16, 
-				16, 					// minimum Y
-				100, 					// maximum Y
-				10 + random.nextInt(4),	// vein size 
-				20);					// tries
+	private void generateOverworld(Random random,
+																 int chunkX,
+																 int chunkZ,
+																 World world,
+																 IChunkGenerator chunkGenerator,
+																 IChunkProvider chunkProvider)
+  {
+		generateOre(ModBlocks.oreManolite.getDefaultState(),
+				world,
+				random,
+				chunkX * 16,
+				chunkZ * 16,
+				RCConfig.oreManoliumMinY,
+				RCConfig.oreManoliumMaxY,
+				RCConfig.oreManoliumVeinSize + random.nextInt(RCConfig.oreManoliumVeinVar),
+				RCConfig.oreManoliumTries);
+
+		generateOre(ModBlocks.oreOctirion.getDefaultState(),
+				world,
+				random,
+				chunkX * 16,
+				chunkZ * 16,
+				RCConfig.oreOctirionMinY,
+				RCConfig.oreOctirionMaxY,
+				RCConfig.oreOctirionVeinSize + random.nextInt(RCConfig.oreOctirionVeinVar),
+				RCConfig.oreOctirionTries);
 	}
 
-	private void generateOre(IBlockState ore, World world, Random random, int x, int z, int minY, int maxY, int size,
-			int chances) {
+	private void generateOre(IBlockState ore,
+													 World world,
+													 Random random,
+													 int x,
+													 int z,
+													 int minY,
+													 int maxY,
+													 int size,
+													 int chances)
+  {
 		int deltaY = maxY - minY;
 
-		for (int i = 0; i < chances; i++) {
+		for (int i = 0; i < chances; i++)
+		{
 			BlockPos pos = new BlockPos(x + random.nextInt(16), minY + random.nextInt(deltaY), z + random.nextInt(16));
 
 			WorldGenMinable generator = new WorldGenMinable(ore, size);
