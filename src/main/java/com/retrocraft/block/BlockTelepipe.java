ce42eac3637b4038e91b5c085704fb1bc3b94b1f
Diego Darriba <ddarriba@udc.es>
2018-02-05 19:23:30 +0100

    improved teleport pipe

--- src/main/java/com/retrocraft/block/BlockTelepipe.java
+++ src/main/java/com/retrocraft/block/BlockTelepipe.java
@@ -1,35 +1,45 @@
 package com.retrocraft.block;
 
-import net.minecraft.block.BlockDirectional;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.Mirror;
-import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockTelepipe extends BlockBase
 {
-  public static final PropertyDirection FACING = BlockDirectional.FACING;
-
-  public BlockTelepipe(String name) {
+  public static final PropertyBool UP = PropertyBool.create("up");
+  public static final PropertyBool DOWN = PropertyBool.create("down");
+  public static final PropertyBool NORTH = PropertyBool.create("north");
+  public static final PropertyBool SOUTH = PropertyBool.create("south");
+  public static final PropertyBool EAST = PropertyBool.create("east");
+  public static final PropertyBool WEST = PropertyBool.create("west");
+  
+  public BlockTelepipe(String name)
+  {
     super(Material.ROCK, name);
 
     setHardness(3f);
     setResistance(5f);
+
+    this.setDefaultState(this.blockState.getBaseState().withProperty(UP, false).withProperty(DOWN, false)
+        .withProperty(NORTH, false).withProperty(EAST, false).withProperty(SOUTH, false)
+        .withProperty(WEST, false));
   }
 
   @Override
-  public BlockTelepipe setCreativeTab(CreativeTabs tab) {
+  public BlockTelepipe setCreativeTab(CreativeTabs tab)
+  {
     super.setCreativeTab(tab);
     return this;
   }
-  
+
   @Override
   public boolean isOpaqueCube(IBlockState iBlockState)
   {
@@ -41,45 +51,53 @@ public class BlockTelepipe extends BlockBase
   {
     return false;
   }
-  
+
   @Override
-  public IBlockState getStateForPlacement(World world, BlockPos pos,
-      EnumFacing side, float hitX, float hitY, float hitZ, int meta,
-      EntityLivingBase base)
+  public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos)
+  {
+    boolean flagN = this.canConnectTo(worldIn, pos.north(), false);
+    boolean flagE = this.canConnectTo(worldIn, pos.east(), false);
+    boolean flagS = this.canConnectTo(worldIn, pos.south(), false);
+    boolean flagW = this.canConnectTo(worldIn, pos.west(), false);
+    boolean flagD = this.canConnectTo(worldIn, pos.down(), false);
+    boolean flagU = this.canConnectTo(worldIn, pos.up(), true);
+    return state
+        .withProperty(UP, flagU)
+        .withProperty(DOWN, flagD)
+        .withProperty(NORTH, flagN)
+        .withProperty(EAST, flagE).withProperty(SOUTH, flagS).withProperty(WEST, flagW);
+  }
+
+  @Override
+  public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing side, float hitX, float hitY,
+      float hitZ, int meta, EntityLivingBase base)
   {
     return this.getStateFromMeta(side.ordinal());
   }
-  
+
   @Override
   public IBlockState getStateFromMeta(int meta)
   {
-    return this.getDefaultState().withProperty(FACING, 
-        EnumFacing.getFront(meta));
+    return this.getDefaultState();
   }
 
   @Override
   public int getMetaFromState(IBlockState state)
   {
-    return state.getValue(FACING).getIndex();
+    return 0;
   }
 
   @Override
   protected BlockStateContainer createBlockState()
   {
-    return new BlockStateContainer(this, FACING);
+    return new BlockStateContainer(this, UP, DOWN, NORTH, EAST, WEST,
+        SOUTH);
   }
 
-  @Override
-  public IBlockState withRotation(IBlockState state, Rotation rot)
+  public boolean canConnectTo(IBlockAccess worldIn, BlockPos pos, boolean up)
   {
-    return state.withProperty(FACING,
-        rot.rotate(state.getValue(FACING)));
-  }
-
-  @Override
-  public IBlockState withMirror(IBlockState state, Mirror mirror)
-  {
-    return this.withRotation(state,
-        mirror.toRotation(state.getValue(FACING)));
+    IBlockState state = worldIn.getBlockState(pos);
+    Block block = state.getBlock();
+    return block == this || (up && block == ModBlocks.blockTeleportPipe);
   }
 }
